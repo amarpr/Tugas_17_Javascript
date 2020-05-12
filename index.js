d0fae6ed6693b5044c8723e4d382cdc23bf54616
Muhamar <amararrasyid@gmail.com>
2020-05-12 17:55:35 +0700

    Tugas 17 Javascript
    Mengumpulkan Tugas Niomic

---
+++ index.js
@@ -0,0 +1,12 @@
+function ubah() {
+    var kalimatSatu = "Saya beLajar bahaSa peMrograman deNgan khUsuk";
+    var kalimatDua = "Allen SedAng MemAncing IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual";
+
+    console.log(kalimatSatu.toLowerCase());
+    console.log(kalimatSatu.toUpperCase());
+
+    console.log(kalimatDua.toLowerCase());
+    console.log(kalimatDua.toUpperCase());
+}
+
+ubah()
